b8bf6549cbf7503827a2734b199a7b3d8285216b
Volcann <mohsinmuhammad823@gmail.com>
2025-01-21 16:45:14 +0500

    Added GET, POST, DELETE, UPDATE methods

--- todo/project/app/urls.py
+++ todo/project/app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
      path('', views.apioverview, name='api-overview'),
      path('task/', views.todolist, name='task-list'),
+     path('task/<str:pk>/', views.taskdetail, name='task-detail'),
+     path('task-create/', views.taskcreate, name='task-create'),
+     path('task-update/<str:pk>/', views.taskupdate, name='task-update'),
+     path('task-del/<str:pk>/', views.taskdelete, name='task-del'),
 ]
--- todo/project/app/views.py
+++ todo/project/app/views.py
@@ -30,3 +30,40 @@ def todolist(request):
     tasks = TODO.objects.all()  # Accessing the 'TODO' model
     serializer = TODOSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskdetail(request, pk):
+    tasks = TODO.objects.get(id=pk)  # Accessing the 'TODO' model
+    serializer = TODOSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskcreate(request):
+    serializer = TODOSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)  # Status 201 for created
+    return Response(serializer.errors, status=400)  # Status 400 for bad request
+
+@api_view(['POST'])
+def taskupdate(request, pk):
+    tasks = TODO.objects.get(id=pk)
+    serializer = TODOSerializer(instance=tasks, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)  # Status 201 for created
+    return Response(serializer.errors, status=400)  # Status 400 for bad request
+
+@api_view(['DELETE'])
+def taskdelete(request, pk):
+    try:
+        # Fetch the task to be deleted
+        task = TODO.objects.get(id=pk)
+    except TODO.DoesNotExist:
+        return Response({"error": "Task not found"}, status=404)
+
+    # Delete the task
+    task.delete()
+    return Response({"message": "Task deleted successfully!"}, status=204) 
